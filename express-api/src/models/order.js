b2e4b3f61f8ab48ca1ee20739201e70ea6a3d166
AnupaGurung <anupagurung111@gmail.com>
2025-10-21 16:11:22 +0545

    Deployment fixes

--- express-api/src/models/order.js
+++ express-api/src/models/order.js
@@ -88,6 +88,6 @@ const orderSchema = new mongoose.Schema({
   timestamps: true
 });
 
-const Order = mongoose.model('Order', orderSchema);
+const Order = mongoose.model('order', orderSchema);
 
 export default Order;
