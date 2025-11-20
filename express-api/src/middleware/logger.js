1c4915416a70c8ad3a627262f76e911c79a95b89
Anupa Gurung <51390617+anupagurung@users.noreply.github.com>
2025-11-20 22:06:18 +0545

    Delete express-api directory

--- express-api/src/middleware/logger.js
+++
@@ -1,7 +0,0 @@
-// src/middleware/logger.js
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.originalUrl}`);
-  next();
-};
-
-export default logger;  // ✅ ESM default export
